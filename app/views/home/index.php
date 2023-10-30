9646afb6ff0c6282c07877527d62c5adb730db8f
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-30 12:55:34 +0800

    a

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ?>
 
@@ -12,3 +12,74 @@ session_start();
         </div>
     </div>
 </header>
+
+<section>
+    <div class="d-flex justify-content-center mt-5">
+        <h1 class="text-center p-2" style="color:white; background-color:blue; width:25%; border-radius:10px;">Top Selling Product</h1>
+    </div>
+
+    <div class="product">
+        <div class="design-content d-flex flex-wrap">
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="fa fa-heart"></i></span>
+                    <span>Bayam</span>
+                </div>
+                                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> </span>
+                    <span>Kangkung</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> </span>
+                    <span>Durian</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> </span>
+                    <span>Mangga</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> </span>
+                    <span>Semangka</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+            <div class="design-item">
+                <div class="design-img">
+                    <img src="<?= BASEURL ?>/img/kangkung.jpg" alt="" class="img-fluid" height="400px">
+                    <span><i class="far fa-heart"></i> </span>
+                    <span>Jeruk</span>
+                </div>
+                <div class="design-title">
+                    <a href="#">Jeruk</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
