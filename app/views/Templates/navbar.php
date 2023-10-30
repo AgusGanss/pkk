a3af475ce8991973fe35bbcf1d4655899f0e2e95
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-10-30 11:16:06 +0800

    all

--- app/views/Templates/navbar.php
+++ app/views/Templates/navbar.php
@@ -1,36 +1,30 @@
-<!-- Nav tabs -->
-<ul class="nav nav-tabs" id="navId" role="tablist">
-    <li class="nav-item">
-        <a href="#tab1Id" class="nav-link active" data-bs-toggle="tab" aria-current="page">Active</a>
-    </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#tab2Id">Action</a>
-            <a class="dropdown-item" href="#tab3Id">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#tab4Id">Action</a>
+<nav class="navbar">
+  <div class="content">
+    <div class="logo">
+      <a href="<?= BASEURL ?>">TOSEMSA</a>
+    </div>
+    <ul class="menu-list">
+      <div class="icon cancel-btn">
+        <i class="fas fa-times"></i>
+      </div>
+      <li><a href="<?= BASEURL ?>" class="nav-list">Home</a></li>
+      <li><a href="<?= BASEURL ?>/About"  class="nav-list">About</a></li>
+      <li>
+        <div class="dropdown show">
+          <a class="dropdown-toggle nav-list" href="<?= BASEURL ?>/services" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Shop Now
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="<?= BASEURL ?>">Vegetable</a>
+            <a class="dropdown-item" href="<?= BASEURL ?>">Fruit</a>
+          </div>
         </div>
-    </li>
-    <li class="nav-item" role="presentation">
-        <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Another link</a>
-    </li>
-    <li class="nav-item" role="presentation">
-        <a href="#" class="nav-link disabled" data-bs-toggle="tab">Disabled</a>
-    </li>
-</ul>
-
-<!-- Tab panes -->
-<div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
-</div>
-
-<!-- (Optional) - Place this js code after initializing bootstrap.min.js or bootstrap.bundle.min.js -->
-<script>
-    var triggerEl = document.querySelector('#navId a')
-    bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
-</script>
+      </li>
+      <li><a href="<?= BASEURL ?>/contact"  class="nav-list">Contact</a></li>
+      <li><a href="<?= BASEURL ?>/login"  class="nav-list">Login</a></li>
+    </ul>
+    <div class="icon menu-btn">
+      <i class="fas fa-bars"></i>
+    </div>
+  </div>
+</nav>
---
+++ app/views/home/index.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+?>
+
+<header>
+    <div class="banner">
+        <div class="container">
+            <h1 class="banner-title">
+                <span>Temukan Kebutuhan Berkualitas Untuk Dapurmu</span>
+            </h1>
+            <a href="#" class="btn btn-primary h1">Shop Now</a>
+        </div>
+    </div>
+</header>
