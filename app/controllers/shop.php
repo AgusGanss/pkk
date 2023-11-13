7857a2ff3aaebeee31a7d50bd40b3de570b5e414
Leoudah <110877097+Leoudah@users.noreply.github.com>
2023-11-13 11:19:59 +0800

    a

--- app/controllers/shop.php
+++ app/controllers/shop.php
@@ -11,4 +11,15 @@ class shop extends Controller
         $this->view('shop/shop', $data);
         $this->view('templates/footer');
     }
+
+    public function items()
+    {
+        $data['judul'] = 'Shop';
+        $data['product'] = $this->model('shop_model')->getAllProductdata();
+        $data['category']=$this->model('category_model')->getCategory();
+        $this->view('templates/items-header', $data);
+        $this->view('templates/navbar');
+        $this->view('shop/items', $data);
+        $this->view('templates/footer');
+    }
 }
--- app/views/templates/items-header.php
+++ app/views/templates/items-header.php
@@ -5,11 +5,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> <?= $data['judul']; ?></title>
-  <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+  <link rel="stylesheet" href="<?= BASEURL; ?>/css/items.css">
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/navbar.css">
-  <link rel="stylesheet" href="<?= BASEURL; ?>/css/styles.css">
-
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
